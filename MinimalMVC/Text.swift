c503d773fc22a4cafdd934307ba01104d0b43a0c
Yuta Hoshino <ythshn@gmail.com>
2018-12-28 08:58:42 +0900

    add: store

---
+++ MinimalMVC/Text.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct Text: Codable {
+    let rawValue: String
+}
--- MinimalMVC/ViewController.swift
+++ MinimalMVC/ViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let kUserDefaultsKey: String = "text"
+    
     @IBOutlet private var textField: UITextField! {
         didSet {
-            textField = nil
+            textField.text = nil
         }
     }
     
@@ -14,13 +15,49 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func onEditingChanged(_ sender: UITextField) {        
-        label.text = createReversedString(sender.text)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initUI()
     }
     
-    private func createReversedString(_ string: String?) -> String? {
-        guard let string = string else { return nil }
-        return String(string.reversed())
+    @IBAction func onEditingChanged(_ sender: UITextField) {        
+        updateLabelText()
+        
+        guard let string = textField.text else {
+            storeTextIntoRepository(nil)
+            return
+        }
+        let text = Text(rawValue: string)
+        storeTextIntoRepository(text)
     }
 }
 
+private extension ViewController {
+    func initUI() {
+        textField.text = getTextFromRepository()?.rawValue
+        updateLabelText()
+    }
+    
+    func updateLabelText() {
+        label.text = createReversedString(textField.text)
+    }
+
+    func createReversedString(_ string: String?) -> String? {
+        guard let string = string else { return nil }
+        return String(string.reversed())
+    }
+
+    func getTextFromRepository() -> Text? {
+        guard let data = UserDefaults.standard.data(forKey: kUserDefaultsKey) else { return nil }
+        return try? JSONDecoder().decode(Text.self, from: data)
+    }
+    
+    func storeTextIntoRepository(_ text: Text?) {
+        guard let text = text else {
+            UserDefaults.standard.set(nil, forKey: kUserDefaultsKey)
+            return
+        }
+        let data = try? JSONEncoder().encode(text)
+        UserDefaults.standard.set(data, forKey: kUserDefaultsKey)
+    }    
+}
